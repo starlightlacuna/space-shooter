812c270a3676f886c71eb3bb4957c6d91ccc155d
starlightlacuna <169253492+starlightlacuna@users.noreply.github.com>
2024-07-05 17:07:31 -0400

    Implement auto cannon firing

--- scripts/auto_cannon.gd
+++ scripts/auto_cannon.gd
@@ -1,38 +1,61 @@
 extends AnimatedSprite2D
 
-@export var cooldown := 0.5
+@export_range(0.1, 10) var cooldown := 0.5
+@export var projectile_speed := 200
 
 const PROJECTILE_PATH := "res://scenes/auto_cannon_projectile.tscn"
 var is_firing := true
 var can_fire := true
+# We set a minimum to ensure the animation plays smoothly at low fire rates
+var min_fps := 20
 
 @onready var source1 = $ProjectileSource1
 @onready var source2 = $ProjectileSource2
 @onready var cooldown_timer = $Cooldown
+@onready var shot_delay_timer = $ShotDelay
+@onready var projectile_scene = preload(PROJECTILE_PATH)
 
-func _ready():
-	ResourceLoader.load_threaded_request(PROJECTILE_PATH)
+#func _ready():
+	#ResourceLoader.load_threaded_request(PROJECTILE_PATH)
 
-func _process(_delta):
+func _physics_process(_delta):
 	if !(is_firing && can_fire):
 		return
 	
-	#var projectile1 = ResourceLoader.load_threaded_get(PROJECTILE_PATH).instantiate()
-	#projectile1.position = source1.position
+	# Calculate the new speed scale
+	# There are 6 frames in the firing animation, so we divide by cooldown to get the FPS
+	var speed = max(1.0, 6.0 / cooldown / 20.0)
 	
-	#var projectile2 = ResourceLoader.load_threaded_get(PROJECTILE_PATH).instantiate()
-	#projectile2.position = source2.position
+	set_speed_scale(speed)
+	play()
 	
-	play("firing")
+	#fire_from_marker(source1)
 	
 	can_fire = false
 	cooldown_timer.wait_time = cooldown
 	cooldown_timer.start()
 
+func _on_animation_finished():
+	stop()
+
 func _on_cooldown_timeout():
 	can_fire = true
 
+func _on_shot_delay_timeout():
+	fire_from_marker(source2)
 
-func _on_animation_finished():
-	if get_animation() == "firing":
-		play("idle")
+func _on_frame_changed():
+	if !is_firing:
+		return
+	
+	match get_frame():
+		1:
+			fire_from_marker(source1)
+		2:
+			fire_from_marker(source2)
+
+func fire_from_marker(marker: Marker2D):
+	var projectile = projectile_scene.instantiate()
+	add_child(projectile)
+	projectile.position = marker.position
+	projectile.set_speed(projectile_speed)
--- scripts/auto_cannon_projectile.gd
+++ scripts/auto_cannon_projectile.gd
@@ -1,11 +1,25 @@
-extends AnimatedSprite2D
+extends Area2D
+
+class_name AutoCannonProjectile
+
+@export var speed = 200
 
 @onready var damage = $Damage.amount
 
+func _physics_process(delta):
+	#var direction = Vector2.UP.rotated(rotation)
+	
+	# This seems to work just fine?
+	var direction = Vector2.RIGHT
+	position += direction * speed * delta
+
 func _on_hitbox_body_entered(body):
-	print("%s hit something!" % [name])
+	print("%s hit $s!" % [name, body.name])
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	# TODO: Use object pools for projectiles!
 	queue_free()
+
+func set_speed(value):
+	speed = value
