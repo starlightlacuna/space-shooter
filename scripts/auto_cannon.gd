5bb5112d82e7773c8d6158a166d0d191bc5d3b02
starlightlacuna <169253492+starlightlacuna@users.noreply.github.com>
2024-07-07 07:58:13 -0400

    Add Kla'ed Torpedo Ship

--- scripts/auto_cannon.gd
+++ scripts/auto_cannon.gd
@@ -1,20 +1,17 @@
 extends AnimatedSprite2D
 
-@export_range(0.1, 10) var cooldown := 0.5
+@export_range(0.1, 5.0) var cooldown := 0.5
 @export var projectile_speed := 200
 
-const PROJECTILE_PATH := "res://scenes/auto_cannon_projectile.tscn"
 var is_firing := true
 var can_fire := true
-# We set a minimum to ensure the animation plays smoothly at low fire rates
-var min_fps := 20
 var projectiles_node: Node
 
 @onready var source1 = $ProjectileSource1
 @onready var source2 = $ProjectileSource2
 @onready var cooldown_timer = $Cooldown
 @onready var shot_delay_timer = $ShotDelay
-@onready var projectile_scene = preload(PROJECTILE_PATH)
+@onready var projectile_scene = preload("res://scenes/spacecraft/player/auto_cannon_projectile.tscn")
 
 func _physics_process(_delta):
 	if !(is_firing && can_fire):
@@ -22,12 +19,10 @@ func _physics_process(_delta):
 	
 	# Calculate the new speed scale
 	# There are 6 frames in the firing animation, so we divide by cooldown to get the FPS
-	var speed = max(1.0, 6.0 / cooldown / 20.0)
+	var speed = max(1.0, 6.0 / cooldown / 10.0)
 	
 	set_speed_scale(speed)
 	play()
-	
-	#fire_from_marker(source1)
 	
 	can_fire = false
 	cooldown_timer.wait_time = cooldown
@@ -54,7 +49,6 @@ func _on_frame_changed():
 
 func fire_from_marker(marker: Marker2D):
 	var projectile = projectile_scene.instantiate()
-	#get_tree().get_current_scene().add_child(projectile)
 	projectiles_node.add_child(projectile)
 	projectile.global_position = marker.global_position
 	projectile.set_speed(projectile_speed)
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,30 +12,25 @@ enum HealthFrame {
 @export var speed: int = 200
 @export var projectiles_node: Node
 
-@onready var bodySprite = $Body
+var frames: Dictionary = {}
+
+@onready var body_sprite = $Body
 
 func _ready():
 	assert(projectiles_node != null, "Projectiles Node not set!")
 	$AutoCannon.set_projectiles_node(projectiles_node)
-	#var projectiles_node = get_node(projectiles_node_path)
-	#print(projectiles_node.name)
+	
+	frames[HealthFrame.HEALTHY] = preload("res://assets/main_ship/base/Main Ship - Base - Full health.png")
+	frames[HealthFrame.SLIGHTLY_DAMAGED] = preload("res://assets/main_ship/base/Main Ship - Base - Slight damage.png")
+	frames[HealthFrame.DAMAGED] = preload("res://assets/main_ship/base/Main Ship - Base - Damaged.png")
+	frames[HealthFrame.HEAVILY_DAMAGED] = preload("res://assets/main_ship/base/Main Ship - Base - Very damaged.png")
 
 func _physics_process(delta):
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	#if direction != Vector2.ZERO:
-		#print(direction)
 	
 	var next_position: Vector2 = direction * speed * delta
 	
 	move_and_collide(next_position)
 
 func set_frame(frame: HealthFrame) -> void:
-	match frame:
-		HealthFrame.HEALTHY:
-			bodySprite.texture = load("res://assets/Main Ship - Base - Full health.png")
-		HealthFrame.SLIGHTLY_DAMAGED:
-			bodySprite.texture = load("res://assets/Main Ship - Base - Slight damage.png")
-		HealthFrame.DAMAGED:
-			bodySprite.texture = load("res://assets/Main Ship - Base - Damaged.png")
-		HealthFrame.HEAVILY_DAMAGED:
-			bodySprite.texture = load("res://assets/Main Ship - Base - Very damaged.png")
+	body_sprite.texture = frames[frame]
